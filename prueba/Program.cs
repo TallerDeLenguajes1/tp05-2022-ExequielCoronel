689692fa4260e68152447e16e4765f07c233c4be
CoronelExequiel <execoronel14f@gmail.com>
2022-05-17 15:10:21 -0300

    primer commit

---
+++ prueba/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace TallerDeLenguajes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string cadena = "Hola mundo";
+
+            Console.WriteLine("Contenido de mi variable cadena actual:"+cadena+"Ingrese un mensaje: ");
+            cadena = Console.ReadLine();
+            cadena = cadena.ToUpper();
+            Console.WriteLine("Nuevo contenido de la cadena: "+cadena);
+
+            int cantidadCar = cadena.Length;
+            Console.WriteLine("cantidad de caracteres de la cadena: "+cantidadCar+"\nIngrese un numero entero: ");
+            cadena = Console.ReadLine();
+            int entero =Convert.ToInt32(cadena);
+            Console.Write("Numero ingresado: "+cadena);
+        }
+    }
+}
