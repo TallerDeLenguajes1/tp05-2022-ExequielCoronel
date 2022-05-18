9dd875b510826c05c6fb9e93acef3c40de67d79a
CoronelExequiel <execoronel14f@gmail.com>
2022-05-18 11:04:34 -0300

    CalculadoraV2

--- prueba/Program.cs
+++ prueba/Program.cs
@@ -1,24 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+double num,num1,num2;
 
-namespace TallerDeLenguajes
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string cadena = "Hola mundo";
-
-            Console.WriteLine("Contenido de mi variable cadena actual:"+cadena+"\nIngrese un mensaje: ");
-            cadena = Console.ReadLine();
-            cadena = cadena.ToLower();
-            Console.WriteLine("Nuevo contenido de la cadena: "+cadena);
-
-            int cantidadCar = cadena.Length;
-            Console.WriteLine("cantidad de caracteres de la cadena: "+cantidadCar+"\nIngrese un numero entero: ");
-            cadena = Console.ReadLine();
-            int entero =Convert.ToInt32(cadena);
-            Console.Write("Numero ingresado: "+cadena);
-        }
-    }
-}
+Console.WriteLine("Ingrese un numero: ");
+num=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("\n|"+num+"| = "+Math.Abs(num));
+Console.WriteLine("\nEl cuadrado de "+num+" es: "+Math.Pow(num,2));
+Console.WriteLine("\nLa raiz cuadrada de "+num+"es: "+Math.Sqrt(num));
+Console.WriteLine("\nsen("+num+") = "+Math.Sin(num));
+Console.WriteLine("\nCos("+num+") = "+Math.Cos(num));
+Console.WriteLine("La parte entera de "+num+" es: "+Math.Truncate(num));
+Console.WriteLine("Ingrese un numero: ");
+num1=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Ingrese otro numero: ");
+num2=Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("El maximo entre "+num1+" y "+num2+" es: "+Math.Max(num1,num2)+"\n");
+Console.WriteLine("El minimo entre "+num1+" y "+num2+" es: "+Math.Min(num1,num2)+"\n");
